76a7cb545a16430f41fc5959664db3adde883900
Bipin Bhandari <bookbipii@gmail.com>
2017-09-04 16:36:49 +0545

    code format

--- bipin/porcupine_number.cpp
+++ bipin/porcupine_number.cpp
@@ -3,37 +3,36 @@
 
 using namespace std;
 
-bool isPrime(int n){
-    for (int i = 2; i < n; ++i){
-        if (n%i == 0){
-            return false;
-        }
+bool isPrime(int n) {
+  for (int i = 2; i < n; ++i) {
+    if (n % i == 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-int nextPrime(int n){
-    while (true){
-        n = n+1;
-        if (isPrime(n)){
-            return n;
-        }
+int nextPrime(int n) {
+  while (true) {
+    n = n + 1;
+    if (isPrime(n)) {
+      return n;
     }
+  }
 }
 
-bool endsWith9(int n){
-    return n%10 == 9;
-}
+bool endsWith9(int n) { return n % 10 == 9; }
 
 int findPorcupineNumber(int n) {
-    while (true){
-        n = n + 1;
-        
-        if (isPrime(n) && endsWith9(n)){
-            int next = nextPrime(n);
-            if (endsWith9(next)) return n;
-        }
+  while (true) {
+    n = n + 1;
+
+    if (isPrime(n) && endsWith9(n)) {
+      int next = nextPrime(n);
+      if (endsWith9(next))
+        return n;
     }
+  }
 }
 
 int main() {
