59acb36d5e9b15fbaacfb7791093e449e89fc3bd
bittu1028 <bittu1028@gmail.com>
2017-09-05 23:19:44 +0545

    Added question 7

---
+++ bittu/question7_findPorcupineNumber.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+#include <limits.h>
+using namespace std;
+int isPrime(int n) {
+  for(int i=2; i< n/2; i++){
+  	if(n % i == 0 ){
+  			return false;
+  	}
+  }
+  return true;
+}
+
+int findPorcupineNumber(int n) {
+  bool isPorcupineNUmber = false; float porcupineNUmber; bool isNextPrimeNumber = false;
+  do {
+    n++; 
+    if(isPrime(n)) {
+      if(isNextPrimeNumber) {
+        if((n%10) == 9){
+          isPorcupineNUmber =  true;
+        }
+        else {
+          isNextPrimeNumber = false;
+        }
+      }
+      if((n%10) == 9 && !isNextPrimeNumber){
+        isNextPrimeNumber = true;
+        porcupineNUmber = n;
+      }
+    }
+  }while(!isPorcupineNUmber);
+  return porcupineNUmber;
+}
+
+int main() 
+{
+  int x = findPorcupineNumber(139);
+  cout << x;
+  return 0;
+}
